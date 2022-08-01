65d617304b4bb59c468e25be8728cbd417476ee3
ghega <ghega@LAPTOP-HEKB9JI7>
2022-08-01 15:46:04 +0530

    [Prasad] commited at 3.46

---
+++ src/serial1/Practical3.java
@@ -0,0 +1,12 @@
+package serial1;
+
+public class Practical3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("last try "+(100+100));
+	}
+
+}
