748f22ba15559e03c1d56097c6c1dc2f7b775244
ghega <ghega@LAPTOP-HEKB9JI7>
2022-08-01 15:33:40 +0530

    [Prasad] prasad commited 3.32

---
+++ src/serial1/Practical1.java
@@ -0,0 +1,9 @@
+package serial1;
+
+public class Practical1 
+{
+	public static void main(String[] args) {
+		
+		System.out.println(10+20);
+	}
+}
---
+++ src/serial1/practical2.java
@@ -0,0 +1,9 @@
+package serial1;
+
+public class practical2 
+{
+	public static void main(String[] args) {
+		
+		System.out.println(50+50);
+	}
+}
